b9a24e77b2d706fc99c5b6bd5ee8b03cf9cf075c
Sebastian <clevereratom120@gmail.com>
2025-09-03 09:00:54 -0600

    🚧
    We are building the movement with rigidbody

--- Assets/script/MovementPlayer.cs
+++ Assets/script/MovementPlayer.cs
@@ -7,11 +7,20 @@ using UnityEngine;
 //: - Herencia, indica que la clase hereda de otra clase
 //MonoBehaviour - Clase base de la que derivan todas las clases de scripts en Unity
 
+/// <summary>
+/// Sebastian Selvas Garcia 05 Sep 2025
+/// Movimineto del jugador
+/// 
+/// </summary>
+
 public class MovementPlayer : MonoBehaviour
 {
     //Variables
 
     //public - Accesible desde cualquier script y clase
+    public Transform transformPlayer; // Transform - Componente que almacena la posicion, rotacion y escala de un objeto
+    public Rigidbody2D rigidbody2DPlayer; // Rigidbody2D - Componente que permite a un objeto 2D ser afectado por la fisica
+
     //private - Solo accesible desde la misma clase
 
     /*tipos de datos
@@ -33,17 +42,33 @@ public class MovementPlayer : MonoBehaviour
     void Start()
     {
         print("Start inicia aqui");
-
+        rigidbody2DPlayer = GetComponent<Rigidbody2D>(); // GetComponent - Obtiene el componente del tipo especificado si el objeto tiene uno
     }//end start
+
+
 
     //Desde frame 2 hasta que termine el juego
     //Loop que se repite constantemente
     // Update is called once per frame
     void Update()
     {
-        print("Update se llama una vez por frame");
 
-    }//end update
+        //Mover al jugador en el eje X
+        if (Input.GetKey(KeyCode.A)) // Input - Clase que maneja la entrada del usuario, GetKey - Devuelve true mientras se mantenga presionada la tecla especificada
+        {
+            transformPlayer.position += new Vector3(-1, 0, 0) * Time.deltaTime; // Vector3 - Estructura que representa un vector en 3D, Time.deltaTime - Tiempo que ha pasado desde el ultimo frame
+            print("Vamos a la izquierda");
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transformPlayer.position += new Vector3(1, 0, 0) * Time.deltaTime;
+            print("Vamos a la derecha");
+
+        }
+        //end update
+    }
 
 
     //Tasa fija de frames
